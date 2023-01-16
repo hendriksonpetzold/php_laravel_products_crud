dc3227b7b64511dc4abc7af25996145ea962cd0d
Hendrikson Petzold <hendrikson_14@hotmail.com>
2023-01-16 16:09:16 -0300

    feat: home_page with database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $products = Product::all();
+        return view(
+            'home',
+            [
+                'products' => $products
+            ]
+        );
     }
 }
