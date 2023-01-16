7be2945d45329e88cf2424254f314ef63f83f5b8
Hendrikson Petzold <hendrikson_14@hotmail.com>
2023-01-16 14:08:06 -0300

    feat: admin page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AdminProductController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/product', [ProductController::class, 'show']);
+
+Route::get('/admin/products', [AdminProductController::class, 'index']);
